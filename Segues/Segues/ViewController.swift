0f53795926265abf2f83c48c92a5e116e783a534
kay-134 <kaykayc0103@gmail.com>
2020-06-19 00:21:24 -0400

    This is a new commit for what I originally planned to be amended

---
+++ Segues/Segues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Segues
+//
+//  Created by admin on 6/18/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
